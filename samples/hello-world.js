2c3c7ce3c3a1d316e480ac70c3b387f45a50e53b
Tony G. Bolaño <agonzalez@movisat.com>
2020-01-27 15:46:11 +0100

    var vs let

---
+++ samples/hello-world.js
@@ -0,0 +1,8 @@
+var message = 'Hello World!';
+if (message) {
+    var i;
+    for (i = 0; i < 3; i++) {
+        console.log(message + ' ' + i);
+    }
+}
+console.log('Value of i = ' + i);
---
+++ samples/hello-world.ts
@@ -0,0 +1,10 @@
+var message = 'Hello World!';
+
+if (message) {
+    let i:number;
+    for (i=0; i<3; i++) {
+        console.log(message + ' ' + i);
+    }
+}
+
+console.log('Value of i = ' + i);
